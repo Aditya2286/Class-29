843be8a7c8f36a0d6332e26c57f195e86fdbe781
Your Name <you@example.com>
2020-12-03 15:49:09 +0530

    Catapult and RubberBand

--- sketch.js
+++ sketch.js
@@ -36,16 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    console.log(bird);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    strokeWeight(4);
+    strokeWeight(0);
     box1.display();
     box2.display();
     ground.display();
@@ -63,7 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
-    //log6.display();
     slingshot.display();    
 }
 
